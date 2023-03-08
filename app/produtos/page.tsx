85feafa7592a61aee20d5be7a7abb6b4330770cb
Saulo Costa <saulotarsobc@gmail.com>
2023-03-08 11:10:07 -0300

    :fire:

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,13 +1,17 @@
 import Produto from "../Components/Produto";
-import produtos from "./produtos.json";
+// import produtos from "./produtos.json";
 import { ProdutoType } from "@/types";
 
 export default async function page() {
+  const produtos = await (await fetch("http://localhost:4000")).json();
+
+  console.log(produtos);
+
   return (
     <main id="produtos">
       <h1>Página de Produtos</h1>
-      {produtos.map(({ nome, preco }: ProdutoType, index) => (
-        <Produto key={index} nome={nome} preco={preco}/>
+      {produtos.map(({ nome, preco }: ProdutoType, index: any) => (
+        <Produto key={index} nome={nome} preco={preco} />
       ))}
     </main>
   );
