ff5a68978da686b3872d066ce835adb03f69851c
Saulo Costa <saulotarsobc@gmail.com>
2023-03-09 17:09:21 -0300

    :fire:

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,7 @@ export default function head() {
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Entrefios - STM</title>
-      <link rel="icon" href="../public/favicon.png" type="image/png" />
+      <link rel="icon" href="./favicon.png" type="image/png" />
       <meta
         name="description"
         content="Bem-vindo ao nosso site de crochê, onde você pode encontrar uma variedade de produtos feitos com fio de malha. Nossos produtos são cuidadosamente tecidos à mão com habilidade e carinho, resultando em peças únicas e duráveis. Se você está procurando por algo especial para decorar sua casa ou presentear alguém especial, você veio ao lugar certo!"
